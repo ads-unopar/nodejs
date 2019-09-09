6dc5aa0a7ead547071638b4ecbc6db52d4e2e912
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2019-09-09 15:41:35 -0300

    Update api-php.php

--- api-php.php
+++ api-php.php
@@ -2,11 +2,15 @@
 
 header('Content-Type: application/json; charset=utf-8');  
 
-$json = file_get_contents('http://localhost:3000/clientes/9');
-$json_str = json_decode($json);
-$clientes = $json_str['nodes'];
+$url_retorno = file_get_contents('http://localhost:3000/clientes/9');
+// Atribui o conteúdo do arquivo para variável $arquivo
 
-foreach ( $clientes as $cliente ) 
-    { echo $cliente['id']."<br>"; } 
+// Decodifica o formato JSON e retorna um Objeto
+$json = json_decode($url_retorno);
+ 
+// Loop para percorrer o Objeto
+foreach($json->clientes as $registro){
+    echo 'ID: ' . $registro->id . ' - Nome: ' . $registro->nome . '<br>';
+}
 
 ?>
